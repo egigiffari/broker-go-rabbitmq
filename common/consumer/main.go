6ae95b78d238dd0f602b5c62707abc4c84e889ed
egigiffari <egisipayung3@gmail.com>
2024-10-10 13:26:38 +0700

    feat: simulate common use of rabbitmq

---
+++ common/consumer/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"github.com/egigiffari/broker-go-rabbitmq/pkg"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const (
+	EXCHANGE = "sample-exchange"
+	QUEUE    = "sample-queue"
+	CONSUMER = "sample-consumer"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.TODO(), syscall.SIGTERM, syscall.SIGINT)
+	defer cancel()
+	rabbitMQ := getMQ()
+
+	connection, err := rabbitMQ.Connection()
+	if err != nil {
+		panic(fmt.Errorf("rabbitmq failed to connect err: %s", err.Error()))
+	}
+
+	channel, err := connection.Channel()
+	if err != nil {
+		panic(fmt.Errorf("rabbitmq channel not response err: %s", err.Error()))
+	}
+
+	if err := prepareQueue(channel); err != nil {
+		panic(fmt.Errorf("rabbitmq failed declare queue err: %s", err.Error()))
+	}
+
+	fmt.Println("====================")
+	fmt.Println("Consume your message")
+	fmt.Println("====================")
+
+	consumer := getConsumer(rabbitMQ)
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go rabbitMQ.Reconnect(ctx)
+	go consumer.Listen(ctx, &wg)
+
+	<-ctx.Done()
+	cancel()
+	rabbitMQ.Close()
+	wg.Wait()
+	fmt.Println("goodbye")
+}
+
+func getMQ() *pkg.RabbitMQ {
+	config := pkg.Config{
+		Host:              "localhost",
+		Port:              5672,
+		Username:          "guest",
+		Password:          "guest",
+		MaxReconnect:      5,
+		ReconnectInterval: 5,
+	}
+
+	return pkg.NewRabbitMQ(&config)
+}
+
+func prepareQueue(channel *amqp.Channel) error {
+	queue := pkg.Queue{
+		Name:    QUEUE,
+		Durable: true,
+		Args:    amqp.Table{},
+	}
+
+	if err := queue.Declare(channel); err != nil {
+		return err
+	}
+
+	return queue.BindOnExchange(channel, "", EXCHANGE, amqp.Table{})
+}
+
+func getConsumer(rabbitmq *pkg.RabbitMQ) *pkg.Consumer {
+	return &pkg.Consumer{
+		Name:      CONSUMER,
+		Queue:     QUEUE,
+		AuthAck:   true,
+		NumWorker: 1,
+		RabbitMQ:  rabbitmq,
+		Handler: func(message amqp.Delivery) {
+			fmt.Printf("recieved message: %s\n", string(message.Body))
+		},
+	}
+}
---
+++ common/publisher/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os/signal"
+	"syscall"
+
+	"github.com/egigiffari/broker-go-rabbitmq/pkg"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const EXCHANGE = "sample-exchange"
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.TODO(), syscall.SIGTERM, syscall.SIGINT)
+	defer cancel()
+	rabbitMQ := getMQ()
+	connection, err := rabbitMQ.Connection()
+	if err != nil {
+		panic(fmt.Errorf("rabbitmq failed to connect err: %s", err.Error()))
+	}
+	defer connection.Close()
+
+	exchange, err := prepareExchange(rabbitMQ)
+	if err != nil {
+		panic(fmt.Errorf("rabbitmq failed declare exchange err: %s", err.Error()))
+	}
+
+	go rabbitMQ.Reconnect(ctx)
+	var message string
+	fmt.Println("====================")
+	fmt.Println("Publish your message")
+	fmt.Println("====================")
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("goodbye")
+			return
+		default:
+			fmt.Print("pesan :")
+			fmt.Scanf("%s", &message)
+			if message == "" {
+				continue
+			}
+
+			exchange.Publish(ctx, "", false, false, amqp.Publishing{
+				Body:         []byte(message),
+				DeliveryMode: amqp.Persistent,
+			})
+			message = ""
+		}
+	}
+}
+
+func getMQ() *pkg.RabbitMQ {
+	config := pkg.Config{
+		Host:              "localhost",
+		Port:              5672,
+		Username:          "guest",
+		Password:          "guest",
+		MaxReconnect:      5,
+		ReconnectInterval: 5,
+	}
+
+	return pkg.NewRabbitMQ(&config)
+}
+
+func prepareExchange(rabbitMQ *pkg.RabbitMQ) (*pkg.Exchange, error) {
+	exchange := pkg.Exchange{
+		Name:     EXCHANGE,
+		Kind:     amqp.ExchangeDirect,
+		Durable:  true,
+		Args:     amqp.Table{},
+		RabbitMQ: rabbitMQ,
+	}
+
+	if err := exchange.Declare(); err != nil {
+		return nil, err
+	}
+
+	return &exchange, nil
+}
